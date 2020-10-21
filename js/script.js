08d1cd2c8b56f21430cd7cf114baf04293bcae5a
marcopolello <marco-pol@hotmail.it>
2020-10-21 18:27:25 +0200

    generazione biglietto completa

--- js/script.js
+++ js/script.js
@@ -11,11 +11,13 @@
 var genera = document.getElementById('bottone');
 genera.addEventListener('click',
  function(){
+   document.getElementById('nascosto').style.display = "block";
+   // variabili
    var euro = 0.21;
    var km = parseInt(document.getElementById('testokm').value);
    var eta = parseInt(document.getElementById('etapasseggero').value);
    var costoViaggio = euro * km;
-
+   var passeggero = (document.getElementById('nome')).value;
    // SCONTO A SECONDA DELL'ETA DEL VIAGGIATORE
    var costoViaggioMinori = ((costoViaggio - (costoViaggio * 0.2)).toFixed(2));
    var costoViaggioAnziani = ((costoViaggio - (costoViaggio * 0.4)).toFixed(2));
@@ -29,7 +31,13 @@ genera.addEventListener('click',
      output.innerHTML = costoViaggio + "€"
    }
    console.log(output);
+   // stampo input ricevuto dall'utente per nome e cognome
+   document.getElementById('name').innerHTML = passeggero;
+   // output dati biglietto
+   document.getElementById('codice').innerHTML = Math.floor(Math.random() * (100000 - 90000 + 1) + 90000 );
+   document.getElementById('carrozza').innerHTML = Math.floor(Math.random() * 10) + 1 ;
 });
+
 // BOTTONE CHE RESETTA
 var annulla = document.getElementById('cancella');
 annulla.addEventListener('click',
