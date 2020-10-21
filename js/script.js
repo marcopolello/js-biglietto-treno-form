276ea1fb21cfc9ccac8f5972649898ec51279241
marcopolello <marco-pol@hotmail.it>
2020-10-21 16:44:44 +0200

    bottone genera il prezzo del biglietto

--- js/script.js
+++ js/script.js
@@ -6,36 +6,27 @@
 // Categoria selezionata dall’utente
 // Aggiungiamo una piccola animazione al click su “Crea” e “Annulla”, se clicchiamo su annulla dobbiamo ripulire il form.
 
-// VARIABILI, INPUT USER
-var euro = 0.21;
-var costoViaggio;
-var km = parseInt(document.getElementById('testokm'));
-var eta = parseInt(document.getElementById('eta'));
-
-//PREZZO DEL BIGLIETTO A SECONDA DEI KM
-costoViaggio = euro * km;
-console.log(costoViaggio, "costodelbiglietto");
-
-// SCONTO A SECONDA DELL'ETA DEL VIAGGIATORE
-var costoViaggioMinori = (costoViaggio - (costoViaggio * 0.2)).toFixed(2);
-var costoViaggioAnziani = (costoViaggio - (costoViaggio * 0.4)).toFixed(2);
-console.log(costoViaggioMinori, costoViaggioAnziani);
-var output = document.getElementById('biglietto')
-// a seconda dell'età verrà visualizzato un costo biglietto
-if (eta <= 18) {
-  output.innerHTML = costoViaggioMinori + "€"
-} else if (eta >= 65) {
-  output.innerHTML = costoViaggioAnziani + "€"
-} else {
-  output.innerHTML = costoViaggio + "€"
-}
-// PREZZO TOTALE DEL VIAGGIO, OUTPUT
-
 
 // BOTTONE CHE GENERA BIGLIETTO
 var bottone = document.getElementById('bottone');
-
 bottone.addEventListener('click',
  function(){
-  (output.innerHTML).value;
+   var euro = 0.21;
+   var km = parseInt(document.getElementById('testokm').value);
+   var eta = parseInt(document.getElementById('etapasseggero').value);
+   var costoViaggio = euro * km;
+
+
+   // SCONTO A SECONDA DELL'ETA DEL VIAGGIATORE
+   var costoViaggioMinori = ((costoViaggio - (costoViaggio * 0.2)).toFixed(2));
+   var costoViaggioAnziani = ((costoViaggio - (costoViaggio * 0.4)).toFixed(2));
+   var output = document.getElementById('biglietto');
+   // a seconda dell'età verrà visualizzato un costo biglietto
+   if (eta <= 18) {
+     output.innerHTML = costoViaggioMinori + "€"
+   } else if (eta >= 65) {
+     output.innerHTML = costoViaggioAnziani + "€"
+   } else {
+     output.innerHTML = costoViaggio + "€"
+   }
 });
