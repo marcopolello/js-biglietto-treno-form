1b47dc4e3ec420aa3fd8d3c6e7616dd29a0d715b
marcopolello <marco-pol@hotmail.it>
2020-10-21 18:45:11 +0200

    aggiunte 3 selezioni alla fascia d'età

--- js/script.js
+++ js/script.js
@@ -11,11 +11,12 @@
 var genera = document.getElementById('bottone');
 genera.addEventListener('click',
  function(){
+   // il biglietto generato compare al click
    document.getElementById('nascosto').style.display = "block";
    // variabili
    var euro = 0.21;
    var km = parseInt(document.getElementById('testokm').value);
-   var eta = parseInt(document.getElementById('etapasseggero').value);
+   var eta = (document.getElementById('etapasseggero').value);
    var costoViaggio = euro * km;
    var passeggero = (document.getElementById('nome')).value;
    // SCONTO A SECONDA DELL'ETA DEL VIAGGIATORE
@@ -23,12 +24,15 @@ genera.addEventListener('click',
    var costoViaggioAnziani = ((costoViaggio - (costoViaggio * 0.4)).toFixed(2));
    var output = document.getElementById('biglietto');
    // a seconda dell'età verrà visualizzato un costo biglietto
-   if (eta <= 18) {
-     output.innerHTML = costoViaggioMinori + "€"
-   } else if (eta >= 65) {
-     output.innerHTML = costoViaggioAnziani + "€"
+   if (eta <= 18 || eta == 'minorenne') {
+     output.innerHTML = costoViaggioMinori + "€";
+     document.getElementById('offerta').innerHTML = "Sconto <br> Minorenne";
+   } else if (eta >= 65 || eta == 'over > 65') {
+     output.innerHTML = costoViaggioAnziani + "€";
+     document.getElementById('offerta').innerHTML = "Sconto <br> Over 65";
    } else {
-     output.innerHTML = costoViaggio + "€"
+     output.innerHTML = costoViaggio + "€";
+     document.getElementById('offerta').innerHTML = "Nessuno <br> sconto";
    }
    console.log(output);
    // stampo input ricevuto dall'utente per nome e cognome
@@ -42,6 +46,9 @@ genera.addEventListener('click',
 var annulla = document.getElementById('cancella');
 annulla.addEventListener('click',
  function () {
+   // scompare biglietto con annulla
+   document.getElementById('nascosto').style.display = "none";
+   // reset dei campi compilati
    document.getElementById('user').reset();
    document.getElementById('trip').reset();
    document.getElementById('age').reset();
